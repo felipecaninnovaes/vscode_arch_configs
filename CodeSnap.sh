2093ee99948f04afe357a93aca739ea6c36d08e9
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2020-01-15 02:59:55 +0000

    fix vscode bug code

--- CodeSnap.sh
+++ CodeSnap.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-mkdir ~/temp_i
-cd ~/temp_i
-
->>>>>>> 66aa26d0db473f28caa9596512401f31baeafacf
 #-> YAY (AUR suport)
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -17,9 +11,3 @@ sudo systemctl start snapd.socket
 
 #-> VSCode Install
 snap install code --classic
-
-<<<<<<< HEAD
-=======
-cd ..
-rm -r ~/temp_i
->>>>>>> 66aa26d0db473f28caa9596512401f31baeafacf
