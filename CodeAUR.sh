987c0653a13c3a56eed7b5863667b848429cb9f8
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2020-01-15 03:00:56 +0000

    fix vscode bug code

--- CodeAUR.sh
+++ CodeAUR.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-mkdir ~/temp_i
-cd ~/temp_i
-
->>>>>>> 66aa26d0db473f28caa9596512401f31baeafacf
 #-> YAY (AUR suport)
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -11,9 +5,3 @@ makepkg -si
 
 #-> VSCode Install
 yay -S visual-studio-code-bin --noconfirm
-<<<<<<< HEAD
-=======
-
-cd ..
-rm -r ~/temp_i
->>>>>>> 66aa26d0db473f28caa9596512401f31baeafacf
